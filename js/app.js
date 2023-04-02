c75f86c60471f4c7414165362539a3457fd65c9b
simretB05 <happysimret@gmail.com>
2023-04-02 07:57:11 -0600

    updated index html page with images and name

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+
 let pokémon = [
     {
         name: `Pikachu`,
@@ -15,21 +16,46 @@ let pokémon = [
         description: `Charizard, known in Japan as Lizardon, is a Pokémon in Nintendo and Game Freak's Pokémon franchise.`,
         image_url:`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScp_RxQxTGFExOq5HmLVAddzOCiLwZrvrm0jku2N4GSw&s`
     },
-    
-]
+] 
 
 
-let selection_json = JSON.stringify( pokémon );
-Cookies.set( `selection`, selection_json );
 
 function selectPokemon( details ){
-    for ( let i = 0; i < details.length; i++ ){
-     details[i][`target`].getAttribute( `selection_kind` )
+    let new_data = []
+
+    // stored the value of the target and stored it in the selected_pokeman variable
+    let selected_pokeman = details[`target`].getAttribute( `selection_kind` )
+    for ( let x = 0; x < pokémon.length; x++ ){
+        //looped through the pokemon.length and if the name of the target value
+        //which is clicked it not equal to the name comming from the loop
+        // then the the value of pokemon at the location of x is pushed to a new arry
+        // and then  is stringified and stored in the selection_json
+        //and its set in the cookies jar with a cookie name selections
+    
+    
+    if ( selected_pokeman === pokémon[x][`name`] ){
+        new_data.push( pokémon[x] )
+        console.log(new_data)
+        let selection_json = JSON.stringify( new_data );
+    Cookies.set( `selection`, selection_json )
     }
+        
+      
+
+        
+     }
+    
+
 }
+
+
+
+// added an event listener to all the buttons with a loop by frist
+// selection the buttons with querySelectorAll
 let selection_btn = document.querySelectorAll( `.selection` );
 
-for ( let i = 0; i < selection_btn.length; i++ )
-{
+
+
+for ( let i = 0; i < selection_btn.length; i++ ){
     selection_btn[i].addEventListener( `click`, selectPokemon )
 }
--- js/selection.js
+++ js/selection.js
@@ -1,24 +1,29 @@
 
-
+//  selected main with querySelector 
 let get_main = document.querySelector( `#main` )
-let user_selection = Cookies.get(`selection`);
-let selection_data = JSON.parse( user_selection );
-console.log(selection_data)
+let user_selection = Cookies.get( `selection` );
 
-for ( let i = 0; i < selection_data.length; i++ ){
-    if(( selection_data.length === 0)){
+let selection_data = JSON.parse(user_selection );
+
+
+if ( selection_data === undefined )
+{
     get_main.insertAdjacentHTML( `afterbegin`, `<h3>Error Please go back to home page and select Your Pokémon </h3>` )
-
-    }else{
-        get_main.insertAdjacentHTML( `afterbegin`, `<div><img  style=" width:60px; height:60px; " src="${selection_data[i][`image_url`]}></div><div style="width:500px; height:500px; display:grid; place-items:center;"><h1>${selection_data[i][`name`]}</h1><p style="width:40%;">${selection_data[i][`description`]}</p><button class="delete">Delete</button></div>`)
+} else{
+    for ( i = 0; i < selection_data.length;  i++){
+        get_main.insertAdjacentHTML( `afterbegin`, `<div><img  style=" width:60px; height:60px; " src="${ selection_data[i][`image_url`] }></div><div style="width:500px; height:500px; display:grid; place-items:center;"><h1>${selection_data[i][`name`] }</h1><p style="width:40%;">${selection_data[i][`description`] }</p><button class="delete">Delete</button></div>` )
 
     }
+    
 }
+
+
+
  
 
-function deletePokemon( details )
-{
-Cookies.remove(`selection`);
-}
-let delete_button = document.querySelector( `.delete` );
-delete_button.addEventListener(`click`, deletePokemon)
+// function deletePokemon( details )
+// {
+// Cookies.remove(`selection`);
+// }
+// let delete_button = document.querySelector( `.delete` );
+// delete_button.addEventListener(`click`, deletePokemon)
